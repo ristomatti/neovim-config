685506f1481d607f93d676841236de3a1da1d1c9
Ristomatti Airo <ristomatti.airo@gmail.com>
2023-12-19 13:25:11 +0200

    Use both alt and ctrl for navigation, autocomplete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,7 +18,8 @@ cmp.setup({
 		autocomplete = false
 	},
 	mapping = {
-		['C-Space'] = cmp.mapping.complete()
+		['C-Space'] = cmp.mapping.complete(),
+		['M-Space'] = cmp.mapping.complete()
 	}
 })
 cmp.setup.buffer {
@@ -32,7 +33,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<M-Space>'] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = vim.NIL
