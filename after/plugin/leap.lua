310abcd3bbed2715f2bcbf9ebac6d1dc9b40f228
Ristomatti Airo <ristomatti.airo@gmail.com>
2023-12-22 15:55:53 +0200

    Customize leap

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -2,7 +2,20 @@ local leap = require('leap')
 local opts = leap.opts
 
 leap.add_default_mappings()
-
 opts.case_sensitive = true
 
+-- Tweak colors
 vim.api.nvim_set_hl(0, 'LeapLabelPrimary', { fg = 'red' })
+
+-- Disable some default keymaps
+vim.keymap.del({ 'x', 'o' }, 'x')
+vim.keymap.del({ 'x', 'o' }, 'X')
+vim.keymap.del({ 'v' }, 's')
+vim.keymap.del({ 'v' }, 'S')
+
+-- Custom keymaps
+-- vim.keymap.set({ 'o', 'x' }, '<C>x', '<Plug>(leap-forward-till)')
+-- vim.keymap.set({ 'o', 'x' }, '<C>X', '<Plug>(leap-backward-till)')
+
+
+
